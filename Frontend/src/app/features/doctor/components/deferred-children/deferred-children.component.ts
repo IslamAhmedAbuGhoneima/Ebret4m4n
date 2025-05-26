24ebeb20776cdf1ccb3be38e8e22b94a6c6620e1
Doaa Ashraf <doaaelshafei1@gmail.com>
2025-05-26 23:24:09 +0200

    save progress before Angular update

--- Frontend/src/app/features/doctor/components/deferred-children/deferred-children.component.ts
+++ Frontend/src/app/features/doctor/components/deferred-children/deferred-children.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '../../services/doctor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-deferred-children',
@@ -6,4 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './deferred-children.component.html',
   styleUrl: './deferred-children.component.css',
 })
-export class DeferredChildrenComponent {}
+export class DeferredChildrenComponent implements OnInit {
+  searchId: any;
+  filteredData: any;
+  data: any;
+  constructor(private router: Router, private _DoctorService: DoctorService) {}
+ 
+  ngOnInit(): void {
+    this.getChildrenSuspended();
+  }
+
+  getChildrenSuspended() {
+    this._DoctorService.childrenSuspended().subscribe({
+      next: (res) => {
+        this.data = res.data;
+        this.filteredData = this.data;
+      },
+      error: (err) => {},
+    });
+  }
+  onSearch() {
+    const id = this.searchId.trim();
+    if (id) {
+      this.filteredData = this.data.filter((child: any) =>
+        child.childId?.toString().includes(id)
+      );
+    } else {
+      this.filteredData = this.data;
+    }
+  }
+}
